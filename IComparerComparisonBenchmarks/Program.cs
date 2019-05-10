31ab94414e8a3e54ceb49ef3fb0f82927a0af25b
ntr <ntr@ihfood.dk>
2019-05-10 22:04:10 +0200

    Try getting open compare

--- IComparerComparisonBenchmarks/Program.cs
+++ IComparerComparisonBenchmarks/Program.cs
@@ -9,6 +9,13 @@ namespace IComparerComparisonBenchmarks
     {
         static void Main(string[] args)
         {
+            // https://stackoverflow.com/questions/7136615/open-delegate-for-generic-interface-method
+            // 
+            //MethodInfo method = typeof(IComparer<string>)
+            //    .GetMethod("Compare", new Type[] { typeof(string), typeof(string) });
+            //var openCompare = (Func<IComparer<string>, string, string, bool>)
+            //    Delegate.CreateDelegate(typeof(Func<IComparer<string>, string, string, bool>), null, method);
+
             var summaryComparableClassInt32 = BenchmarkRunner.Run<ComparerComparisonBenchmarkComparableClassInt32>();
             var summaryInt = BenchmarkRunner.Run<ComparerComparisonBenchmarkInt>();
             //var summaryString = BenchmarkRunner.Run<ComparerComparisonBenchmarkString>();
